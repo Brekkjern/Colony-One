6bb1b3e30369cbcd624157c0257b18cde8d0b9d4
Nils Kristian Brekke <nils.kristian.brekke@gmail.com>
2015-09-28 22:25:13 +0200

    Added building list to Colony object. Added updating of all buildings.

--- Entities/colony.py
+++ Entities/colony.py
@@ -4,14 +4,21 @@ from Entities.colonist import Colonist
 class Colony(object):
     """Object model for Colony objects"""
 
-    def __init__(self, colonists = None):
+    def __init__(self, colonists=None, buildings=None):
         if not colonists:
-            self.colonists = []
+           colonists = []
+
+        if not buildings:
+            buildings = []
 
         self.colonists = colonists
+        self.buildings = buildings
         self.power = 0
         self.food = 0
 
     def update(self):
         for colonist in self.colonists:
             colonist.update()
+
+        for building in self.buildings:
+            building.update()
