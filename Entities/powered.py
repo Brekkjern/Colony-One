f83e212238fabad90a824474a5193b61aa65f40a
Nils Kristian Brekke <nils.kristian.brekke@gmail.com>
2015-10-03 14:20:01 +0200

    Added power state to class

--- Entities/powered.py
+++ Entities/powered.py
@@ -1,7 +1,8 @@
 class Powered(Object):
     """Object model for powered structures."""
 
-    def __init__(self, colony, passive, active):
+    def __init__(self, colony, passive, active, powered=False):
         self.colony = colony
         self.passive = passive
         self.active = active
+        self.powered = powered
