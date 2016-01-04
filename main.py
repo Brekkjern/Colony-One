6eeeb10bca72e699167327492a2185e92518b16e
Nils Kristian Brekke <nils.kristian.brekke@gmail.com>
2016-01-04 17:41:29 +0100

    Removed useless statements from test code.

--- main.py
+++ main.py
@@ -7,12 +7,10 @@ test_colony = Colony(conf.game_settings)
 for i in range(0, 10):
     # print("DEBUG: Add colonists to colony.")
     test_colony.new_colonist()
-    i += 1
 
 for i in range(0, 10):
     print("DEBUG: Add structure to colony.")
     test_colony.new_building()
-    i += 1
 
 while test_colony.colonists:
     conf.tick += 1
