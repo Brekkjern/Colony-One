65be043d0ee0283d8796d5ec429c4a9e36f28a74
Nils Kristian Brekke <nils.kristian.brekke@gmail.com>
2016-11-20 22:08:29 +0100

    Fleshed out renderer component

--- Components/renderer.py
+++ Components/renderer.py
@@ -1,4 +1,28 @@
+import math
+import weakref
+from typing import List
+
+from common import Point
+
+
 class Renderer(object):
-    def __init__(self, rendering_depth: int, texture):
-        self.rendering_depth = rendering_depth
+    # Table to hold all renderer components.
+    renderers = []  # type: List['Renderer']
+
+    def __init__(self, rendering_layer: int, texture, texture_center_offset: Point, size: Point):
+        self.__class__.renderers.append(weakref.proxy(self))
+        self.rendering_layer = rendering_layer
         self.texture = texture
+        self.texture_center_offset = texture_center_offset
+        self.size = size
+
+    def prepare_render(self, pixel_location: Point) -> 'Renderer':
+        self.pixel_location = pixel_location
+
+        return self
+
+    def get_pixel_location(self) -> Point:
+        posx = math.floor((self.pixel_location.x + self.texture_center_offset.x) * self.size.x)
+        posy = math.floor((self.pixel_location.y + self.texture_center_offset.y) * self.size.y)
+
+        return Point(posx, posy)
