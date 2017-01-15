3109e22f8462adc1e8f058285bfcb9639cc6f3ab
Nils Kristian Brekke <nils.kristian.brekke@gmail.com>
2017-01-15 18:01:58 +0100

    Input value validation for clamp function
    Ensures the input values are valid numbers and that the smaller value isn't bigger than the larger value.
    Solves potential issues where values are input incorrectly.

--- common.py
+++ common.py
@@ -1,4 +1,7 @@
 from collections import namedtuple
+from typing import Union
+
+NumAlias = Union[float, int]
 
 Point = namedtuple("Point", ["x", "y"])
 
@@ -8,5 +11,13 @@ def lerp(start: float, end: float, step: float) -> float:
     return start + (end - start) * step
 
 
-def clamp(value, min_value, max_value):
+def clamp(value: NumAlias, min_value: NumAlias, max_value: NumAlias) -> NumAlias:
+    """ Clamps a value between a minimum and maximum value.
+
+    Errors if the minimum value is larger than the maximum.
+    """
+
+    if min_value > max_value:
+        raise ValueError("Minimum value can't be larger than maximum value.")
+
     return max(min(value, max_value), min_value)
