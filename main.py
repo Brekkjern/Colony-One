cab264d678bba05bad4c1f74679997dfc1e68680
Nils Kristian Brekke <nils.kristian.brekke@gmail.com>
2017-01-22 15:27:11 +0100

    Fjernet samarbeidspartnere fra hjulet

--- main.py
+++ main.py
@@ -1,10 +1,19 @@
 from Entities.hexmap import Axial
+from Entities.hexmap import Map
+from Entities.hexmap import Hex
 
-testAxial = Axial(1, 2)
-anotherAxial = Axial(5, -4)
+tileMap = Map()
+for tile in tileMap.draw_circle(Axial(0, 0), 10000):
+    tile = Hex(tile.q, tile.r)
+    tileMap.add_hex_to_map(tile)
 
-newAxial = testAxial + anotherAxial
-print(newAxial.q, newAxial.r)
+# testAxial = Axial(1, 2)
+# anotherAxial = Axial(5, -4)
+#
+# newAxial = testAxial + anotherAxial
+# print(newAxial.q, newAxial.r)
+#
+# newAxial *= 3
+# print(newAxial.q, newAxial.r)
 
-newAxial *= 3
-print(newAxial.q, newAxial.r)
+
